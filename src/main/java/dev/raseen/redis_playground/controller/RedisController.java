41787f9d6e5f2d934cbf45aeb93b033086e27361
raseen2004 <raseen429@gmail.com>
2026-06-01 10:53:39 +0530

    Hash_practice

--- src/main/java/dev/raseen/redis_playground/controller/RedisController.java
+++ src/main/java/dev/raseen/redis_playground/controller/RedisController.java
@@ -1,13 +1,16 @@
 package dev.raseen.redis_playground.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.raseen.redis_playground.service.RedisService;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequestMapping("/redis")
@@ -18,7 +21,7 @@ public class RedisController {
         this.redisService = redisService;
     }
 
-    @GetMapping("/set/{name}")    
+    @GetMapping("/set/{name}")
     public String setName(@PathVariable String name) {
         redisService.saveName(name);
         return "Saved" + name;
@@ -27,5 +30,22 @@ public class RedisController {
     @GetMapping("/get")
     public String getName() {
         return redisService.getName();
+    }
+
+    @PostMapping("/user/{id}")
+    public String createUser(@PathVariable Long id, @RequestParam String name, @RequestParam String age,
+            @RequestParam String city) {
+        redisService.saveUser(id, name, age, city);
+        return "User" + name + "Saved";
+    }
+
+    @GetMapping("/user/{id}")
+    public Map<Object, Object> getUser(@PathVariable Long id) {
+        return redisService.getUsers(id);
+    }
+
+    @GetMapping("/user/{id}/{field}")
+    public Object getField(@PathVariable Long id, @PathVariable String field) {
+        return redisService.getUserFields(id, field);
     }
 }
